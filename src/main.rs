25e2a31675ea8e3e571aed6347fdb10d74a0d33a
Sandipsinh Rathod <zotbysandip@gmail.com>
2023-10-08 19:12:59 +0530

    miscellaneous

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,7 @@ fn main() {
     let mut output = std::io::stdout().lock();
     let prompt_password = &RpasswordPromptPassword;
     if std::env::args().len() == 1 {
-        repl(
-            &mut input,
-            &mut output,
-            prompt_password,
-            DEFAULT_PASSWORD_FILENAME.to_string(),
-        )
+        repl(&mut input,&mut output,prompt_password,DEFAULT_PASSWORD_FILENAME.to_string())
     } else {
         let args = Args::parse();
         run_cli(&mut input, &mut output, prompt_password, args);
